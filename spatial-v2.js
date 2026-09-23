2737436c1044ca2effe5d5c7d9eb80d3e57f349f
Miljenka Ćurković <miljenka.cur@gmail.com>
2026-09-23 08:08:35 -0700

    Use temporally refined packed alpha in spatial AR

--- spatial-v2.js
+++ spatial-v2.js
@@ -1,6 +1,6 @@
 import * as THREE from 'https://cdn.jsdelivr.net/npm/three@0.183.2/build/three.module.js'
 window.THREE=THREE
-const PACKED_URL='./assets/diocletian-packed-alpha.mp4'
+const PACKED_URL='./assets/diocletian-packed-alpha-temporal-h264.mp4?v=20260923'
 const GARDEN_START=5.75
 let video,figure,shadow,xrCamera,statusTimer,anchored=true
 const $=id=>document.getElementById(id)
